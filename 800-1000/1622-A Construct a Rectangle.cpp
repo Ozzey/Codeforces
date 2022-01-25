935e69ac16b60b9e8569ce3273016320f984dd99
Aditya Narendra <49760167+Ozzey@users.noreply.github.com>
2022-01-26 03:10:52 +0530

    Initial Commit

---
+++ 800-1000/1622-A Construct a Rectangle.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int tc;
+    cin >> tc;
+    int l1,l2,l3;
+    int i=0;
+
+    while(i < tc){
+        cin>>l1>>l2>>l3;
+
+        if(abs(l1-l2)==l3 || abs(l1-l3)==l2 || abs(l2-l3)==l1)
+            cout<<"yes"<<endl;
+        else if(l1%2==0 && l2==l3 || l2%2==0 && l1==l3 || l3%2==0 && l1==l2)
+            cout << "yes"<<endl;
+        else {
+            cout << "no"<<endl;
+        }
+        i++;
+        }
+
+    return 0;
+
+}
